827378570c1b510c9ad5568e90b84e8180cdf16a
soumanta-yugen <soumanta&yugen.ai>
2020-12-28 23:54:36 +0530

    rf in loops, updated cml report

--- train.py
+++ train.py
@@ -26,64 +26,69 @@ X_train, X_test, y_train, y_test = train_test_split(
 #################################
 
 # Fit a model on the train section
-regr = RandomForestRegressor(max_depth=5, random_state=seed)
-regr.fit(X_train, y_train)
+max_depth = [5, 10, 15]
+for depth in max_depth:
 
-# Report training set score
-train_score = regr.score(X_train, y_train) * 100
-# Report test set score
-test_score = regr.score(X_test, y_test) * 100
+    regr = RandomForestRegressor(max_depth=5, random_state=seed)
+    regr.fit(X_train, y_train)
 
-# Write scores to a file
-with open("metrics.txt", "w") as outfile:
-    outfile.write("Training variance explained: %2.1f%%\n" % train_score)
-    outfile.write("Test variance explained: %2.1f%%\n" % test_score)
+    # Report training set score
+    train_score = regr.score(X_train, y_train) * 100
+    # Report test set score
+    test_score = regr.score(X_test, y_test) * 100
 
-
-##########################################
-##### PLOT FEATURE IMPORTANCE ############
-##########################################
-# Calculate feature importance in random forest
-importances = regr.feature_importances_
-labels = df.columns
-feature_df = pd.DataFrame(
-    list(zip(labels, importances)), columns=["feature", "importance"]
-)
-feature_df = feature_df.sort_values(by="importance", ascending=False,)
-
-# image formatting
-axis_fs = 18  # fontsize
-title_fs = 22  # fontsize
-sns.set(style="whitegrid")
-
-ax = sns.barplot(x="importance", y="feature", data=feature_df)
-ax.set_xlabel("Importance", fontsize=axis_fs)
-ax.set_ylabel("Feature", fontsize=axis_fs)  # ylabel
-ax.set_title("Random forest\nfeature importance", fontsize=title_fs)
-
-plt.tight_layout()
-plt.savefig("feature_importance.png", dpi=120)
-plt.close()
+    # Write scores to a file
+    metrics_filename = "metrics_" + str(depth) + ".txt"
+    with open(metrics_filename, "w") as outfile:
+        outfile.write("Training variance explained: %2.1f%%\n" % train_score)
+        outfile.write("Test variance explained: %2.1f%%\n" % test_score)
 
 
-##########################################
-############ PLOT RESIDUALS  #############
-##########################################
+    ##########################################
+    ##### PLOT FEATURE IMPORTANCE ############
+    ##########################################
+    # Calculate feature importance in random forest
+    # importances = regr.feature_importances_
+    # labels = df.columns
+    # feature_df = pd.DataFrame(
+    #     list(zip(labels, importances)), columns=["feature", "importance"]
+    # )
+    # feature_df = feature_df.sort_values(by="importance", ascending=False,)
 
-y_pred = regr.predict(X_test) + np.random.normal(0, 0.25, len(y_test))
-y_jitter = y_test + np.random.normal(0, 0.25, len(y_test))
-res_df = pd.DataFrame(list(zip(y_jitter, y_pred)), columns=["true", "pred"])
+    # # image formatting
+    axis_fs = 18  # fontsize
+    title_fs = 22  # fontsize
+    # sns.set(style="whitegrid")
 
-ax = sns.scatterplot(x="true", y="pred", data=res_df)
-ax.set_aspect("equal")
-ax.set_xlabel("True wine quality", fontsize=axis_fs)
-ax.set_ylabel("Predicted wine quality", fontsize=axis_fs)  # ylabel
-ax.set_title("Residuals", fontsize=title_fs)
+    # ax = sns.barplot(x="importance", y="feature", data=feature_df)
+    # ax.set_xlabel("Importance", fontsize=axis_fs)
+    # ax.set_ylabel("Feature", fontsize=axis_fs)  # ylabel
+    # ax.set_title("Random forest\nfeature importance", fontsize=title_fs)
 
-# Make it pretty- square aspect ratio
-ax.plot([1, 10], [1, 10], "black", linewidth=1)
-plt.ylim((2.5, 8.5))
-plt.xlim((2.5, 8.5))
+    # plt.tight_layout()
+    # plt.savefig("feature_importance.png", dpi=120)
+    # plt.close()
 
-plt.tight_layout()
-plt.savefig("residuals.png", dpi=120)
+
+    ##########################################
+    ############ PLOT RESIDUALS  #############
+    ##########################################
+
+    y_pred = regr.predict(X_test) + np.random.normal(0, 0.25, len(y_test))
+    y_jitter = y_test + np.random.normal(0, 0.25, len(y_test))
+    res_df = pd.DataFrame(list(zip(y_jitter, y_pred)), columns=["true", "pred"])
+
+    ax = sns.scatterplot(x="true", y="pred", data=res_df)
+    ax.set_aspect("equal")
+    ax.set_xlabel("True wine quality", fontsize=axis_fs)
+    ax.set_ylabel("Predicted wine quality", fontsize=axis_fs)  # ylabel
+    ax.set_title("Residuals", fontsize=title_fs)
+
+    # Make it pretty- square aspect ratio
+    ax.plot([1, 10], [1, 10], "black", linewidth=1)
+    plt.ylim((2.5, 8.5))
+    plt.xlim((2.5, 8.5))
+
+    plt.tight_layout()
+    residuals_imgname = "residuals_" + str(depth) + ".png"
+    plt.savefig(residuals_imgname, dpi=90)
